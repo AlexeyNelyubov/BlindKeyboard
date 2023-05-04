fcc68170edb6af69df944d519d3192e2f542399f
AlexeyNelyubov <ANelyubov93@gmail.com>
2023-05-04 13:09:29 +0300

    refactoring TestPage

--- src/views/TestPage.vue
+++ src/views/TestPage.vue
@@ -3,34 +3,19 @@ import { ref } from "vue";
 import { useRouter } from "vue-router";
 import Timer from "/src/components/TestPage/Timer.vue";
 import TestingParams from "/src/components/TestPage/TestingParams.vue";
+import { getRandomTextFromAjax } from "/src/helpers/getRandomTextFromAjax.js";
 import { useEventListener } from "/src/composable/useEventListener.js";
 import { useTestingParamsStore } from "/src/stores/testingParams.js";
 
 const testingParamsStore = useTestingParamsStore();
 
 const router = useRouter();
-const DEFAULT_TEXT =
-  "Земля — пятая по величине планета Солнечной системы и единственная, имеющая слой газов в атмосфере, гарантирующий условия для существования здесь жизни. Около 70% поверхности планеты покрыто водой в жидком состоянии, необходимым элементом для существования форм жизни.";
-const randomText = ref("");
-const getRandomTextFromAjax = async () => {
-  try {
-    const response = await fetch(
-      "https://fish-text.ru/get?&tipe=sentence&number=1"
-    );
-    const json = await response.json();
-    if (response.ok) {
-      randomText.value = json.text;
-    } else {
-      console.log(json);
-      randomText.value = DEFAULT_TEXT;
-    }
-  } catch (error) {
-    console.error(error.message);
-    randomText.value = DEFAULT_TEXT;
-  }
-};
 
-getRandomTextFromAjax();
+const randomText = ref("");
+
+(async () => {
+  randomText.value = await getRandomTextFromAjax();
+})();
 
 const curentIndex = ref(0);
 const isCurentSymbolValid = ref(true);
