eba21519e6148c5e11b5464a136b31ee3eb7f71f
AlexeyNelyubov <ANelyubov93@gmail.com>
2023-05-14 16:15:20 +0300

    refactoring

---
+++ src/views/ResultPage.vue
@@ -0,0 +1,28 @@
+<script setup>
+import { useRouter } from "vue-router";
+import Header from "/src/components/IndexPage/Header.vue";
+import Information from "/src/components/IndexPage/Information.vue";
+import StartTest from "/src/components/IndexPage/StartTest.vue";
+
+const router = useRouter();
+
+const beginTest = () => {
+  router.push({ name: "Test" });
+};
+</script>
+
+<template>
+  <div class="index">
+    <Header />
+    <Information />
+    <StartTest @start-test="beginTest" />
+  </div>
+</template>
+
+<style>
+.index {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+</style>
